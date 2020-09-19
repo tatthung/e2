bf54202afec7d92be645fd0adff52266364e733f
Tatt Hung <tlhung@hotmail.com>
2020-09-19 15:52:59 +0800

    Added bank.php as practice/index.php

---
+++ practice/index2.php
@@ -0,0 +1,14 @@
+<!doctype html> 
+ <html lang='en'> 
+  <head>
+    <title>Practice Application</title>     
+   <meta charset='utf-8'> 
+  </head> 
+ <body>     
+  <h1>Practice Application</h1>
+    <img src='/images/hes-logo.png' alt='HES Logo'> 
+
+  <p>Lorem ipsum dolor met.
+  <footer>&copy; 2020</footer>
+ </body> 
+</html>
