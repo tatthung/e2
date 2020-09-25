911b33589de4716cf20b21f18bf1e92d22480e1a
Tatt Hung <tlhung@hotmail.com>
2020-09-25 17:42:48 +0800

    Added week4 practices

---
+++ week4/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$moves = ['rock', 'paper', 'scissors']; #Arrar of Strings
+# #0=rock, #1=paper, #3=scissors
+$strawLengs = [2, 2, 2, 2, 2, 1];
+
+$mixed = ['rock', 1, .04, true];
+
+echo $moves[0]; #rock
