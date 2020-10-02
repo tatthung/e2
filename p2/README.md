27cdb0d6f36fb173096ae8813de0504ad23864cd
Tatt Hung <tlhung@hotmail.com>
2020-10-02 11:47:03 +0800

    Built Project 2

---
+++ p2/README.md
@@ -0,0 +1,15 @@
+# Project 1 
+# Advance Rock, Scissors and Paper Game 
++ By: Tatt Loong Hung 
++ Production URL: <http://e2p1.tatthung.me>  
+
+## Outside resources 1. 
+xxx
+
+## Notes for instruction 
+1. This is the modified version of the conventional rock-scissors-paper game. 
+2. As an additional challenge for myself to understand and learn the If and Else loop in depth, I have slightly increase the complexity 
+of the game to make it more challenging by including If and Else loop within existing If and Else loop where in the case of a tie, both players get another 
+chance where they can use their other hand to throw moves, and that will determine the winner for that round. 
+3. I have implemented an If and Else loop on 
+index-view.php to publish (echo) the second-hand game results (after tie).
---
+++ p2/index.php
@@ -0,0 +1,81 @@
+<?php
+
+$moves = ['rock', 'scissors', 'paper']; # Build an array to introduce Rock, Scissors and Paper
+
+# Randomly set Player A and B's move - either rock, scissors or paper
+$playerA = $moves[rand(0, 2)];
+$playerB = $moves[rand(0, 2)];
+$playerASecondHand = $moves[rand(0, 2)];
+$playerBSecondHand = $moves[rand(0, 2)];
+
+# Rock beats Scissors, Scissors beats Paper, Paper beats Rock.
+if ($playerA == $moves[0] and $playerB == $moves[1]) {
+    $winner = 'Player A';
+} elseif ($playerA == $moves[1] and $playerB == $moves[2]) {
+    $winner = 'Player A';
+} elseif ($playerA == $moves[2] and $playerB == $moves[0]) {
+    $winner = 'Player A';
+} else {
+    $winner = 'Player B';
+}
+
+# In the case scenario where Tie happens
+# Both players will get a second chance to throw new moves with their second hand
+# Winning the second round refers to winning that overall round
+if ($playerA == $moves[0] and $playerB == $moves[0]) {
+    $winner = 'Tie';
+} elseif ($playerASecondHand == $moves[0] and $playerBSecondHand == $moves[0]) {
+    $winnerTwo = 'Tie';
+} elseif ($playerASecondHand == $moves[0] and $playerBSecondHand == $moves[1]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[1] and $playerBSecondHand == $moves[1]) {
+    $winnerTwo = 'Tie';
+} elseif ($playerASecondHand == $moves[1] and $playerBSecondHand == $moves[2]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[2] and $playerBSecondHand == $moves[0]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[2] and $playerBSecondHand == $moves[2]) {
+    $winnerTwo = 'Tie';
+} else {
+    $winnerTwo = 'Player B';
+}
+
+# Second Tie scenario
+if ($playerA == $moves[1] and $playerB == $moves[1]) {
+    $winner = 'Tie';
+} elseif ($playerASecondHand == $moves[0] and $playerBSecondHand == $moves[0]) {
+    $winnerTwo = 'Tie';
+} elseif ($playerASecondHand == $moves[0] and $playerBSecondHand == $moves[1]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[1] and $playerBSecondHand == $moves[1]) {
+    $winnerTwo = 'Tie';
+} elseif ($playerASecondHand == $moves[1] and $playerBSecondHand == $moves[2]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[2] and $playerBSecondHand == $moves[0]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[2] and $playerBSecondHand == $moves[2]) {
+    $winnerTwo = 'Tie';
+} else {
+    $winnerTwo = 'Player B';
+}
+
+# Third Tie scenario
+if ($playerA == $moves[2] and $playerB == $moves[2]) {
+    $winner = 'Tie';
+} elseif ($playerASecondHand == $moves[0] and $playerBSecondHand == $moves[0]) {
+    $winnerTwo = 'Tie';
+} elseif ($playerASecondHand == $moves[0] and $playerBSecondHand == $moves[1]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[1] and $playerBSecondHand == $moves[1]) {
+    $winnerTwo = 'Tie';
+} elseif ($playerASecondHand == $moves[1] and $playerBSecondHand == $moves[2]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[2] and $playerBSecondHand == $moves[0]) {
+    $winnerTwo = 'Player A';
+} elseif ($playerASecondHand == $moves[2] and $playerBSecondHand == $moves[2]) {
+    $winnerTwo = 'Tie';
+} else {
+    $winnerTwo = 'Player B';
+}
+
+require 'index-view.php';
